bb0d3bf407fd41c416dad747d956365ef4a3b96c
Dielison <153321263+XimiLoko@users.noreply.github.com>
2024-01-14 23:42:27 -0300

    Update index.js
     em processo de finalização

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ class game{
 
 }
 
-/*class Heroi {
+class Heroi {
   constructor(nome, idade, tipo) {
     this.nome = nome;
     this.idade = idade;
@@ -80,14 +80,3 @@ class game{
       default:
         ataque = "usou um ataque genérico";
     }
-
-    console.log(`O ${this.tipo} atacou usando ${ataque}`);
-  }
-}
-
-// Exemplo de uso da classe
-const heroi1 = new Heroi("Herói 1", 30, "mago");
-const heroi2 = new Heroi("Herói 2", 25, "guerreiro");
-
-heroi1.atacar(); // Saída: O mago atacou usando magia
-heroi2.atacar(); // Saída: O guerreiro atacou usando espada
